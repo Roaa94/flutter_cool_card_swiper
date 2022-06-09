c7833e93150cfbb6889a4c34156ad9499a1fc56b
roaa94 <roaa94khaddam@gmail.com>
2022-06-10 01:07:53 +0300

    refactor and add comments to CoolSwiperCard widget

--- lib/src/widgets/cool_swiper_card.dart
+++ lib/src/widgets/cool_swiper_card.dart
@@ -18,21 +18,100 @@ class CoolSwiperCard extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<CoolSwiperCard> createState() => _CardState();
+  State<CoolSwiperCard> createState() => _CoolSwiperCardState();
 }
 
-class _CardState extends State<CoolSwiperCard>
+class _CoolSwiperCardState extends State<CoolSwiperCard>
     with SingleTickerProviderStateMixin {
   late final AnimationController animationController;
 
-  late Animation<double> rotationAnimation;
+  late final Animation<double> rotationAnimation;
   late final Animation<double> slideUpAnimation;
-  late Animation<double> slideDownAnimation;
+  late final Animation<double> slideDownAnimation;
   late final Animation<double> scaleAnimation;
+
+  Tween<double> rotationAnimationTween = Tween<double>(begin: 0, end: -360);
+  Tween<double> slideDownAnimationTween = Tween<double>(begin: 0, end: 0);
 
   double yDragOffset = 0;
   double dragStartAngle = 0;
   Alignment dragStartRotationAlignment = Alignment.centerRight;
+  Duration dragDuration = const Duration(milliseconds: 0);
+
+  /// When the drag starts, the card rotates a small angle
+  /// with an alignment based on the touch/click location of the user
+  ///
+  /// And the main flying rotation tween gets its end value based on the
+  /// touch/click location as well to determine whether the flying flip will
+  /// happen with a negative or positive angle
+  void _onVerticalDragStart(DragStartDetails details) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    final xPosition = details.globalPosition.dx;
+    final yPosition = details.localPosition.dy;
+    final angleMultiplier = xPosition > screenWidth / 2 ? -1 : 1;
+    rotationAnimationTween.end =
+        Constants.rotationAnimationAngleDeg * angleMultiplier;
+
+    // Update values of the small angle drag start rotation animation
+    setState(() {
+      dragStartRotationAlignment = getDragStartPositionAlignment(
+        xPosition,
+        yPosition,
+        screenWidth,
+        Constants.cardHeight,
+      );
+      dragStartAngle = Constants.dragStartEndAngle * angleMultiplier;
+      // If the drag duration is larger than zero, rest to zero
+      // to allow the card to move with user finger/mouse smoothly
+      if (dragDuration > Duration.zero) {
+        dragDuration = Duration.zero;
+      }
+    });
+  }
+
+  /// When the drag ends, first a check is made to ensure the card travelled some
+  /// offset distance upwards,
+  /// if it didn't, the cards returns to place
+  /// if it did, the animation is triggered by
+  ///   - calling a callback to the parent widget
+  ///   - changing the end value of the slide down animation tween
+  ///     based on how much distance the card travelled
+  ///   - calling forward() on the animation controller
+  ///
+  /// After the animation finishes, a callback to the parent widget is
+  /// called to let it know that it can swap the background cards and brings
+  /// them forward to reset the indices and allow for the next card to be dragged & animated
+  void _onVerticalDragEnd(DragEndDetails details) {
+    if ((yDragOffset * -1) > Constants.initAnimationOffset) {
+      widget.onAnimationTrigger();
+      slideDownAnimationTween.end = Constants.throwSlideYDistance +
+          yDragOffset.abs() -
+          (widget.card.totalCount - 1) * Constants.yOffset;
+
+      animationController.forward().then((value) {
+        widget.onVerticalDragEnd();
+        setState(() {
+          dragStartAngle = 0;
+        });
+      });
+    } else {
+      setState(() {
+        // Set a non-zero drag rotation to allow the card to reset to original
+        // position smoothly rather than snapping back into place
+        dragDuration = const Duration(milliseconds: 200);
+        yDragOffset = 0;
+        dragStartAngle = 0;
+      });
+    }
+  }
+
+  /// This moves the card with user touch/click & hold
+  void _onVerticalDragUpdate(DragUpdateDetails details) {
+    setState(() {
+      yDragOffset += details.delta.dy;
+    });
+  }
 
   @override
   void initState() {
@@ -42,10 +121,7 @@ class _CardState extends State<CoolSwiperCard>
       duration: Constants.swipeAnimationDuration,
     );
 
-    rotationAnimation = Tween<double>(
-      begin: 0,
-      end: -360,
-    ).animate(CurvedAnimation(
+    rotationAnimation = rotationAnimationTween.animate(CurvedAnimation(
       parent: animationController,
       curve: Curves.easeInOut,
     ));
@@ -55,6 +131,8 @@ class _CardState extends State<CoolSwiperCard>
       end: 1 - ((widget.card.totalCount - 1) * Constants.scaleFraction),
     ).animate(animationController);
 
+    // Staggered animation is used here to allow
+    // sequencing the slide up & slide down animations
     slideUpAnimation = Tween<double>(
       begin: 0,
       end: -Constants.throwSlideYDistance,
@@ -63,16 +141,9 @@ class _CardState extends State<CoolSwiperCard>
       curve: const Interval(0, 0.5, curve: Curves.linear),
     ));
 
-    slideDownAnimation = Tween<double>(
-      begin: 0,
-      end: 0,
-    ).animate(CurvedAnimation(
+    slideDownAnimation = slideDownAnimationTween.animate(CurvedAnimation(
       parent: animationController,
-      curve: const Interval(
-        0.5,
-        1,
-        curve: Curves.linear,
-      ),
+      curve: const Interval(0.5, 1, curve: Curves.linear),
     ));
   }
 
@@ -84,89 +155,51 @@ class _CardState extends State<CoolSwiperCard>
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    // print('Rebuilt card: ${widget.card.order}');
-
     return GestureDetector(
-      onVerticalDragStart: (DragStartDetails details) {
-        final xPosition = details.globalPosition.dx;
-        final yPosition = details.localPosition.dy;
-        // print('drag start global x position & local y positions [$xPosition, $yPosition]');
-        final isAngleNegative = xPosition > screenWidth / 2;
-        rotationAnimation = Tween<double>(
-          begin: 0,
-          end: Constants.rotationAnimationAngleDeg * (isAngleNegative ? -1 : 1),
-        ).animate(animationController);
-
-        setState(() {
-          dragStartRotationAlignment = getDragStartPositionAlignment(
-            xPosition,
-            yPosition,
-            screenWidth,
-            Constants.cardHeight,
-          );
-          dragStartAngle =
-              Constants.dragStartEndAngle * (isAngleNegative ? -1 : 1);
-        });
-      },
-      onVerticalDragUpdate: (DragUpdateDetails details) {
-        setState(() {
-          yDragOffset += details.delta.dy;
-        });
-      },
-      onVerticalDragEnd: (DragEndDetails details) {
-        if (yDragOffset.abs() > Constants.initAnimationOffset) {
-          widget.onAnimationTrigger();
-          slideDownAnimation = Tween<double>(
-            begin: 0,
-            end: Constants.throwSlideYDistance +
-                yDragOffset.abs() -
-                (widget.card.totalCount - 1) * Constants.yOffset,
-          ).animate(CurvedAnimation(
-            parent: animationController,
-            curve: const Interval(0.5, 1, curve: Curves.linear),
-          ));
-
-          animationController.forward().then((value) {
-            setState(() {
-              widget.onVerticalDragEnd();
-              dragStartAngle = 0;
-            });
-          });
-        } else {
-          setState(() {
-            yDragOffset = 0;
-            dragStartAngle = 0;
-          });
-        }
-      },
+      onVerticalDragStart: _onVerticalDragStart,
+      onVerticalDragUpdate: _onVerticalDragUpdate,
+      onVerticalDragEnd: _onVerticalDragEnd,
       child: TweenAnimationBuilder<double>(
         tween: Tween<double>(begin: 0, end: yDragOffset),
-        duration: const Duration(milliseconds: 100),
+        duration: dragDuration,
         curve: Curves.easeOut,
+        // This TweenAnimationBuilder widget is responsible for the user
+        // touch/click & hold dragging
+        // Or the DRAG UPDATE ANIMATION
         builder: (c, double value, child) => Transform.translate(
           offset: Offset(0, value),
           child: child,
         ),
         child: AnimatedBuilder(
           animation: animationController,
-          child: widget.card.child,
+          // This widgets is responsible for the small angle rotation
+          // triggered on user touch/click & hold
+          // Or the DRAG START ANIMATION
+          child: AnimatedRotation(
+            turns: dragStartAngle,
+            alignment: dragStartRotationAlignment,
+            duration: const Duration(milliseconds: 200),
+            child: widget.card.child,
+          ),
           builder: (c, child) {
+            // This widgets inside the builder method of the AnimatedBuilder
+            // widget are responsible for the:
+            // slide-up => rotation => slide-down animations
+            // Or the DRAG END ANIMATION
             return Transform.translate(
+              // slide up some distance beyond drag location
               offset: Offset(0, slideUpAnimation.value),
               child: Transform.translate(
+                // slide down into place
                 offset: Offset(0, slideDownAnimation.value),
                 child: Transform.rotate(
+                  // rotate
                   angle: rotationAnimation.value * (math.pi / 180),
                   alignment: Alignment.center,
                   child: Transform.scale(
+                    // Scale down to scale of the smallest card in stack
                     scale: scaleAnimation.value,
-                    child: AnimatedRotation(
-                      turns: dragStartAngle,
-                      alignment: dragStartRotationAlignment,
-                      duration: const Duration(milliseconds: 200),
-                      child: child,
-                    ),
+                    child: child,
                   ),
                 ),
               ),
