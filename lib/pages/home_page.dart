aab8e0a186f3005da1271fcc2b6519b1b10c6d98
roaa94 <roaa94khaddam@gmail.com>
2022-06-10 00:09:09 +0300

    extract CardContent widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_card_swiper/constants.dart';
 import 'package:flutter_card_swiper/data.dart';
+import 'package:flutter_card_swiper/widgets/card_content.dart';
 import 'package:flutter_card_swiper/widgets/cool_swiper.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,55 +16,7 @@ class HomePage extends StatelessWidget {
           child: CoolSwiper(
             children: List.generate(
               Data.colors.length,
-              (index) => Container(
-                height: Constants.cardHeight,
-                padding: const EdgeInsets.all(40),
-                decoration: BoxDecoration(
-                  color: Data.colors[index],
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.2),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                      const SizedBox(width: 15),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 15,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Container(
-                            height: 15,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              (index) => CardContent(color: Data.colors[index]),
             ),
           ),
         ),
---
+++ lib/widgets/card_content.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/constants.dart';
+
+class CardContent extends StatelessWidget {
+  final Color color;
+
+  const CardContent({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Constants.cardHeight,
+      padding: const EdgeInsets.all(40),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.2),
+                shape: BoxShape.circle,
+              ),
+            ),
+            const SizedBox(width: 15),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 15,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  height: 15,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
