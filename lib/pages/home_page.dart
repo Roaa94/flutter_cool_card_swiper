d1aabd2c54b91cbbd3a6190add7a7ca6c40555aa
roaa94 <roaa94khaddam@gmail.com>
2022-06-09 12:42:19 +0300

    add card content

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,9 +18,51 @@ class HomePage extends StatelessWidget {
               Data.colors.length,
               (index) => Container(
                 height: Constants.cardHeight,
+                padding: const EdgeInsets.all(40),
                 decoration: BoxDecoration(
                   color: Data.colors[index],
                   borderRadius: BorderRadius.circular(18),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.2),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 15,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            height: 15,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
